f240d7a1388a18e9d7db32d0f9b698614f166ca6
Pulak Kumar Das <pulak1901@gmail.com>
2020-03-13 17:27:51 +0530

    Use proper functions instead of function variables so argument names are useful.

--- brotli/decoder/oneshot_decoder.dart
+++ brotli/decoder/oneshot_decoder.dart
@@ -17,20 +17,32 @@ typedef brotli_decoder_decompress_nt = Int32 Function(
 
 class BrotliOneshotDecoder 
 {
-  int Function() version;
+  int Function() _version;
   int Function(
+    int, Pointer<Uint8>,
+    Pointer<Int32>, Pointer<Uint8>
+  ) _decompress;
+
+  BrotliOneshotDecoder(DynamicLibrary dynamicLibrary) {
+    _version = dynamicLibrary
+      .lookup<NativeFunction<brotli_decoder_version_nt>>('BrotliDecoderVersion')
+      .asFunction();
+    _decompress = dynamicLibrary
+      .lookup<NativeFunction<brotli_decoder_decompress_nt>>('BrotliDecoderDecompress')
+      .asFunction();
+  }
+
+  /// Get the version of the library
+  int version() => _version();
+
+  /// Performs one-shot memory-to-memory decompression.
+  /// 
+  /// Decompresses the data in [encoded_buffer] into [decoded_buffer], and sets
+  /// [decoded_size] to the decompressed length.
+  int decompress(
     int encoded_size,
     Pointer<Uint8> encoded_buffer,
     Pointer<Int32> decoded_size,
     Pointer<Uint8> decoded_buffer
-  ) decompress;
-
-  BrotliOneshotDecoder(DynamicLibrary dynamicLibrary) {
-    version = dynamicLibrary
-      .lookup<NativeFunction<brotli_decoder_version_nt>>('BrotliDecoderVersion')
-      .asFunction();
-    decompress = dynamicLibrary
-      .lookup<NativeFunction<brotli_decoder_decompress_nt>>('BrotliDecoderDecompress')
-      .asFunction();
-  }
+  ) => _decompress(encoded_size, encoded_buffer, decoded_size, decoded_buffer);
 }
--- brotli/encoder/oneshot_encoder.dart
+++ brotli/encoder/oneshot_encoder.dart
@@ -17,19 +17,32 @@ typedef brotli_encoder_compress_nt = Int32 Function(
 
 class BrotliOneshotEncoder
 {
-  int Function() version;
+  int Function() _version;
   int Function(
-    int quality, int lgwin, int mode,
-    int input_size, Pointer<Uint8> input_buffer,
-    Pointer<Int32> encoded_size, Pointer<Uint8> encoded_buffer
-  ) compress;
+    int, int, int,
+    int, Pointer<Uint8>,
+    Pointer<Int32>, Pointer<Uint8>
+  ) _compress;
 
   BrotliOneshotEncoder(DynamicLibrary dynamicLibrary) {
-    version = dynamicLibrary
+    _version = dynamicLibrary
       .lookup<NativeFunction<brotli_encoder_version_nt>>('BrotliEncoderVersion')
       .asFunction();
-    compress = dynamicLibrary
+    _compress = dynamicLibrary
       .lookup<NativeFunction<brotli_encoder_compress_nt>>('BrotliEncoderCompress')
       .asFunction();
   }
+
+  /// Gets the version of the library
+  int version() => _version();
+
+  /// Performs one-shot memory-to-memory compression.
+  /// 
+  /// Compresses the data in [input_buffer] into [encoded_buffer], and sets
+  /// [encoded_size] to the compressed length.
+  int compress(
+    int quality, int lgwin, int mode,
+    int input_size, Pointer<Uint8> input_buffer,
+    Pointer<Int32> encoded_size, Pointer<Uint8> encoded_buffer
+  ) => _compress(quality, lgwin, mode, input_size, input_buffer, encoded_size, encoded_buffer);
 }
