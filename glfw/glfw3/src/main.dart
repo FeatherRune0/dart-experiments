51c00a717e476ac025dbaeee8a31169785c6f4f0
Pulak Kumar Das <pulak1901@gmail.com>
2020-03-19 16:29:08 +0530

    Initialize glfw program structure

---
+++ glfw/glfw3/src/main.dart
@@ -0,0 +1,11 @@
+/// Main functions and definitions needed to use glfw3
+
+// TODO: glfwInit()
+// TODO: glfwTerminate()
+// TODO: glfwSetErrorCallback()
+// TODO: glfwCreateWindow()
+// TODO: glfwMakeContextCurrent()
+// TODO: glfwWindowShouldClose()
+// TODO: glfwGetFramebufferSize()
+// TODO: glfwSwapBuffers()
+// TODO: glfwPollEvents()
---
+++ glfw/simple.dart
@@ -0,0 +1,6 @@
+/// recreation of https://www.glfw.org/docs/3.3/quick_guide.html
+
+import 'glfw3/glfw3.dart';
+
+main() {
+}
